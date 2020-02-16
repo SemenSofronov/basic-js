ec05904897b43b2ba7d4030eaccfee28798da522
sofronov <studentssau6@gmail.com>
2020-02-17 03:19:52 +0400

    feat: do simple-chain function

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,34 @@
 const chainMaker = {
-  getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  }
+    chain: [],
+    getLength() {
+        this.chain.length;
+    },
+    addLink(value) {
+        let temp = String(value);
+        if (temp.includes("function (")) temp = temp.replace("function (", "function(");
+        this.chain.push(String(temp));
+        return this;
+    },
+    removeLink(position) {
+        if (typeof position !== "number" || position - 1 < 0 || position - 1 > this.chain.length - 1) {
+            this.chain = [];
+            throw new Error();
+        }
+        this.chain.splice(position - 1, 1);
+        return this;
+    },
+    reverseChain() {
+        this.chain = this.chain.reverse();
+        return this;
+    },
+    finishChain() {
+        let result = this.chain;
+        for (let i = 0; i < result.length; i++) {
+            result[i] = '( ' + result[i] + ' )';
+        }
+        this.chain = [];
+        return result.join("~~");
+    }
 };
 
 module.exports = chainMaker;
